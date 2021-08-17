c07015ffad974b062cccbded4cfcf90540de400c
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-08-17 15:40:29 -0300

    Exercicio de condições

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,83 @@
+// EXERCICIO DE INTERPRETAÇÃO
+
+// 1. A) O codio vai mostrar o resto da divisão do numero que o usuario colocar. Se o resultado for igual a 0 ira passar.
+// B) Números pares.
+// C) Números impares.
+
+// 2. A) Mostrar ao usuario qual o preço da fruta que ele digitou.
+// B) O preço da fruta maçã é R$ 2.25
+// C) ira mostrar os dois preços : 5.5 e 5
+
+// 3. A) declarando uma constante que vai guardar o valor que o usuario digitar
+// B) Esse número passou no teste. Não vai retornar nada
+// C) ira dar erro no console pois a mensagem nao esta gravada no escopo global.
+
+// EXERCICIO DE ESCRITRA
+
+// 1.
+// const idadeUsuario = Number(prompt("Qual sua idade?"))
+
+// if (idadeUsuario > 18) {
+//     console.log("Você pode dirigir")
+// } else {
+//     console.log("Você não pode dirigir")
+// }
+
+// 2. 
+// const horarioUsuario = prompt("Qual horario você estuda? M / V / N?").toLowerCase()
+
+// if (horarioUsuario === 'm' ) {
+//     console.log("Bom Dia!")
+// } else if (horarioUsuario === 'v') {
+//     console.log("Boa Tarde!")
+// } else if (horarioUsuario === 'n') {
+//     console.log("Boa Noite!")
+// } else {
+//     console.log("Escolha uma das opções válidas: M, V ou N")
+// }
+
+// 3.
+// const horarioUsuario = prompt("Qual horario você estuda? M / V / N?").toLowerCase()
+
+// switch (horarioUsuario) {
+//     case 'm': 
+//         console.log("Bom Dia!")
+//         breakm
+//     case 'v':
+//         console.log("Boa Tarde!")
+//         break
+//     case 'n': 
+//         console.log("Boa Noite!")
+//         break
+//     default:
+//         console.log("Escolha uma das opções válidas: M, V ou N")
+//         break
+// }
+
+// 4.
+// const generoFilme = prompt("Qual o genero do filme?").toLowerCase()
+
+// const valorDoIngresso = Number(prompt("Qual o valor do ingresso?"))
+
+// if (generoFilme === 'fantasia' && valorDoIngresso < 15) {
+//     console.log("Bom filme!")
+// } else {
+//     console.log("Escolha outro filme :(")
+// }
+
+// DESAFIO
+
+// 1.
+// const generoFilme = prompt("Qual o genero do filme?").toLowerCase()
+
+// const valorDoIngresso = Number(prompt("Qual o valor do ingresso?"))
+
+// const lanchinho = prompt("O que vocês vão comer?")
+
+// if (generoFilme === 'fantasia' && valorDoIngresso < 15) {
+//     console.log("Bom filme!", " Aproveite o seu ", lanchinho)
+// } else {
+//     console.log("Escolha outro filme :(")
+// }
+
+
