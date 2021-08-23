b4b901c8bbd00eb24429530dab39d2696a80b60e
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-08-23 16:34:37 -0300

    Maior Numero

--- semana3/revisao-js/lista-revisao-js/exercicios.js
+++ semana3/revisao-js/lista-revisao-js/exercicios.js
@@ -36,6 +36,12 @@ function retornaNumerosParesElevadosADois(array) {
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
   
+    let maior = - Infinity
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maior) {
+            maior = array[i]}
+    }
+return maior
 }
 
 // EXERCÍCIO 07
