9fc2919250a9c3aa5af74a2079b0bfa1e80a1ce4
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-09-10 18:01:13 -0300

    Insta4

--- semana5/insta4/insta4/src/App.js
+++ semana5/insta4/insta4/src/App.js
@@ -9,44 +9,125 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const DivInputs = styled.div`
+  border: 1px solid gray;
+  display: grid;
+  align-items: center;
+  row-gap: 8px;
+  
+  width: 290px;
+  margin: 10px 0;
+  padding: 10px 5px;
+
+  input:hover {
+    background-color: lightsalmon;
+  }
+
+  button:hover {
+    background-color: darkgray;
+    color: whitesmoke;
+    border: white;
+    border-radius: 2px;
+    height: 20px;
+  }
+
+  button:active {
+    background-color: lightskyblue;
+  }
+
+`
+
 class App extends React.Component {
     state = {
       posts: [
         { 
           usuario: "paulinha",
           fotoPerfil: 'https://picsum.photos/50/50?random=1',
-          post: "https://picsum.photos/200/150?random=1"
+          fotoPost: "https://picsum.photos/200/150?random=1"
         },
         {
           usuario: "fotos_lindas",
           fotoPerfil: "https://picsum.photos/50/50?random=2",
-          post: "https://picsum.photos/200/150?random=2"
+          fotoPost: "https://picsum.photos/200/150?random=2"
         },
         {
           usuario: "rogerinho",
           fotoPerfil: "https://picsum.photos/50/50?random=3",
-          post: "https://picsum.photos/200/150?random=3"
+          fotoPost: "https://picsum.photos/200/150?random=3"
         }
+      ],
 
-      ]
-    }
+          // valores Input
+          inputUsuario: "",
+          inputFotoPerfil: "",
+          inputFotoPost: ""
+        
+    };
+
+    onClickAdicionaPost = () => {
+      const novoPost = {
+        usuario: this.state.inputUsuario,
+        fotoUsuario: this.state.inputFotoPerfil,
+        fotoPost: this.state.inputFotoPost
+      }
+
+      const novosPosts = [... this.state.posts, novoPost]
+
+      this.setState({
+        posts: novosPosts,
+        inputUsuario: "",
+        inputFotoPerfil: "",
+        inputFotoPost: ""
+      })
+    };
+
+    onChangeInputUsuario = (event) => {
+      this.setState({inputUsuario: event.target.value})
+    };
+
+    onChangeInputFotoPerfil = (event) => {
+      this.setState({inputFotoPerfil: event.target.value})
+    };
+
+    onChangeInputFotoPost = (event) => {
+      this.setState({inputFotoPost: event.target.value})
+    };
+
   render() { 
 
     const listaDeComponentes = this.state.posts.map((post) => {
       return (
-        <MainContainer>
           <Post
           nomeUsuario = {post.usuario}
           fotoUsuario = {post.fotoPerfil}
-          fotoPost = {post.post}
+          fotoPost = {post.fotoPost}
           />
-        </MainContainer>
       );
     });
 
     return (
       <MainContainer>
-        {listaDeComponentes}
+        <DivInputs>
+        <input
+            value={this.state.inputUsuario}
+            onChange={this.onChangeInputUsuario}
+            placeholder={"Nome do Usuário"}
+          />
+
+        <input
+            value={this.state.inputFotoPerfil}
+            onChange={this.onChangeInputFotoPerfil}
+            placeholder={"Foto do Perfil"}
+          />
+
+        <input
+            value={this.state.inputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder={"Link da foto"}
+          />
+          <button onClick={this.onClickAdicionaPost}>Adicionar Foto</button>
+        </DivInputs>
+        <div> {listaDeComponentes} </div>
       </MainContainer>
     );
   }
