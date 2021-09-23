b4d551c4f7dea8879041a5006770ae6042d449fa
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-09-23 18:26:32 -0300

    API publicas

---
+++ semana7/apis-publicas/exercicios-aula/exercicios/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GetRandomActivity from "./components/GetRandomActivity.js";
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <GetRandomActivity/>
+      </div>
+    );
+  }
+}
