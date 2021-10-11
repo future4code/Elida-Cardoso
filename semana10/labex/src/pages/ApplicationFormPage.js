59993cf513084a7aebb79db03794fd13e9f36e05
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-10-11 17:39:29 -0300

    Pages do projeto

---
+++ semana10/labex/src/pages/ApplicationFormPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ApplicationFormPage = () => {
+    return (
+        <div>
+            ApplicationFormPage
+        </div>
+    )
+}
+
+export default ApplicationFormPage;
