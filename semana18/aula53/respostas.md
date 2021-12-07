77c1a4c940b5035ea66774fe7f166f4152281652
Elida Nunes Cardoso <elidanccardoso@gmail.com>
2021-12-07 17:02:40 -0300

    exercício herança

---
+++ semana18/aula53/respostas.md
@@ -0,0 +1,15 @@
+# Exercício 1 
+
+A) Não.
+
+B) Sempre que for criado um novo user.
+
+# Exercício 2
+
+A) Uma
+
+B) Uma, pois ela também e chamada na construção da Costumer, que é uma extensão da User.
+
+# Exercício 2
+
+A) Não, pois ela é privada e não tem nenhum getter.
---
+++ semana18/aula53/src/index.ts
@@ -0,0 +1,23 @@
+import express, {Express} from 'express'
+import cors from 'cors'
+import { AddressInfo } from "net";
+import { Customer, User } from "./types"
+
+const app: Express = express();
+
+const newUser1: User = new User("001", "elida@gmail.com", "Élida", "123456")
+
+const newUser2: Customer = new Customer("002", "astrodev@gmail.com", "Astrodev", "123456", "125478536145")
+
+console.log(newUser2.getId(), newUser2.getEmail(), newUser2.getName(), newUser2.getCreditCard())
+
+console.log(newUser2.introduceYourself())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
